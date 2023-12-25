8313ea81aab362a61561307bdcb475132ecbf6a3
Matheus Gama <68973867+mth-gama@users.noreply.github.com>
2023-12-25 11:16:52 +0000

    :up: upload the files

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask ,jsonify ,render_template
+
+
+api = Flask(__name__)
+
+
+
+@api.route('/')
+def home():
+    return render_template('index.html')
+
+
+@api.route('/confidencial')
+def response_estaplanetar():
+    with open('x.txt','r') as arquivoX:
+        confidencial = arquivoX.read()
+
+    with open('loc.txt','r') as arquivoloc:
+        loc = arquivoloc.read()
+    
+    with open('exploracao.txt','r') as exp:
+        expExpedicao = exp.read()
+
+    dados ={'confidencial':confidencial,'localizacao':loc,'expedicao':expExpedicao}
+
+    return jsonify(dados)
+api.run(port=5000,host='localhost',debug=True)
